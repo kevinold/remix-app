8a8066db7ba1ed6883b32d19b3ae506fcc8c82c6
Kevin Old <kevin@kevinold.com>
2024-01-25 11:23:38 -0600

    add shadcn-ui button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -22,6 +23,7 @@ export default function Index() {
     <div>
       <div>Todo: { todo.title }</div>
       <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
+      <Button>Click me</Button>
       <ul>
         <li>
           <a
